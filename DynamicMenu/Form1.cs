54dd821527292f99cacbdb35572fc3b524de2425
현호 김 <hyunho.kim@hanwha-qcells.com>
2018-09-06 21:46:54 +0900

    new comit

--- DynamicMenu/Form1.cs
+++ DynamicMenu/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -26,10 +27,10 @@ namespace DynamicMenu
         void InitMenu()
         {
             MenuItems = new List<Menu>();
-            MenuItems.Add(new Menu() { ID = "1", Code = "1", MenuType = 1, Name = "File", OrderBy = 1, ParentID = "", Tag = "", Enabled=true });
-            MenuItems.Add(new Menu() { ID = "2", Code = "Item", MenuType = 2, Name = "Item", OrderBy = 2, ParentID = "1", Tag = "", Enabled = true });
-            MenuItems.Add(new Menu() { ID = "3", Code = "Custom", MenuType = 2, Name = "Custom", OrderBy = 2, ParentID = "1", Tag = "", Enabled = true });
-            MenuItems.Add(new Menu() { ID = "4", Code = "Exit", MenuType = 2, Name = "Exit", OrderBy = 2, ParentID = "1", Tag = "", Enabled = true });
+            MenuItems.Add(new Menu() { ID = "1", Code = "File", MenuType = 1, Name = "File", OrderBy = 1, ParentID = "", Tag = "", Enabled = true, Visiable = true, DllName= "DynamicMenu.exe", FormName="frmItem", NameSpace ="DynamicMenu" });
+            MenuItems.Add(new Menu() { ID = "2", Code = "Item", MenuType = 2, Name = "Item", OrderBy = 2, ParentID = "1", Tag = "", Enabled = false, Visiable = true, DllName = "DynamicMenu.exe", FormName = "frmItem", NameSpace = "DynamicMenu" });
+            MenuItems.Add(new Menu() { ID = "3", Code = "Custom", MenuType = 2, Name = "Custom", OrderBy = 3, ParentID = "1", Tag = "", Enabled = true, Visiable = true, DllName = "DynamicMenu.exe", FormName = "frmCustom", NameSpace = "DynamicMenu" });
+            MenuItems.Add(new Menu() { ID = "4", Code = "Exit", MenuType = 2, Name = "Exit", OrderBy = 4, ParentID = "1", Tag = "", Enabled = true, Visiable = true, DllName = "DynamicMenu.exe", FormName = "frmItem", NameSpace = "DynamicMenu" });
 
 
             var mns = MenuItems.Where(f => f.ParentID == "").ToList();
@@ -46,7 +47,7 @@ namespace DynamicMenu
                     mn.Click += Mn_Click;
                     mn.Enabled = menu.Enabled;
                     this.menuStrip.Items.Add(mn);
-                    var mns = MenuItems.Where(f => f.ParentID == menu.ID).ToList();
+                    var mns = MenuItems.Where(f => f.ParentID == menu.ID && f.Visiable == true).OrderBy(f => f.OrderBy).ToList();
                     CreateMenuItem(mn, mns);
                 }
             }
@@ -55,14 +56,13 @@ namespace DynamicMenu
         private void CreateMenuItem(ToolStripMenuItem MenuItem, List<Menu> menus)
         {
             foreach (var menu in menus)
-            {
-                
-                    ToolStripMenuItem mn = new ToolStripMenuItem(menu.Name);
-                    mn.Click += Mn_Click;
-                    mn.Enabled = menu.Enabled;
-                    mn.Tag = menu;
-                    MenuItem.DropDownItems.Add(mn);
-                
+            {                
+                ToolStripMenuItem mn = new ToolStripMenuItem(menu.Name);
+                mn.Click += Mn_Click;
+                mn.Enabled = menu.Enabled;
+                mn.Visible = menu.Visiable;
+                mn.Tag = menu;
+                MenuItem.DropDownItems.Add(mn);                
             }
         }
 
@@ -78,6 +78,35 @@ namespace DynamicMenu
                     mn.Enabled = menu.Enabled;
                     menuItem.DropDownItems.Add(mn);
                 }
+            }
+        }
+
+        private void ExecuteMenu(Menu menu)
+        {
+            try
+            {
+
+                Assembly assembly = Assembly.LoadFrom(Application.StartupPath + "\\" + menu.DllName);
+                if(assembly == null)
+                {
+                    MessageBox.Show("Assembly is null : " + menu.DllName);
+                    return;
+                }
+                Type type = assembly.GetType(menu.NameSpace + "." + menu.FormName);
+                if(type == null)
+                {
+                    MessageBox.Show("Type is null : " + menu.DllName);
+                    return;
+                }
+                Form form = (Form)Activator.CreateInstance(type);
+              //  form.MdiParent = this;
+                form.Text = menu.Name;
+                form.Tag = menu;
+                form.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception : " + ex.Message);
             }
         }
 
@@ -97,6 +126,7 @@ namespace DynamicMenu
                                 Close();
                                 break;
                             default:
+                                ExecuteMenu(menu);
                                 break;
                         }
                     }
--- DynamicMenu/Menu.cs
+++ DynamicMenu/Menu.cs
@@ -8,15 +8,30 @@ namespace DynamicMenu
 {
     public class Menu
     {
-        public string ID;
-        public string Code;
-        public int MenuType;
-        public string Name;
-        public int OrderBy;
-        public string ParentID;
-        public string Tag;
-        public bool Enabled;
-        public string NameSpace;
-        public string FormName;
+        private string iD;
+        private string code;
+        private int menuType;
+        private string name;
+        private int orderBy;
+        private string parentID;
+        private string tag;
+        private bool enabled;
+        private string nameSpace;
+        private string formName;
+        private string dllName;
+        private bool visiable;
+
+        public string ID { get => iD; set => iD = value; }
+        public string Code { get => code; set => code = value; }
+        public int MenuType { get => menuType; set => menuType = value; }
+        public string Name { get => name; set => name = value; }
+        public int OrderBy { get => orderBy; set => orderBy = value; }
+        public string ParentID { get => parentID; set => parentID = value; }
+        public string Tag { get => tag; set => tag = value; }
+        public bool Enabled { get => enabled; set => enabled = value; }
+        public string NameSpace { get => nameSpace; set => nameSpace = value; }
+        public string FormName { get => formName; set => formName = value; }
+        public string DllName { get => dllName; set => dllName = value; }
+        public bool Visiable { get => visiable; set => visiable = value; }
     }
 }
